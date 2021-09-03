d071c09ee55f52d101df67a885d00b5f6aaad798
anisahasna <anisa.hasna@intern.bukalapak.com>
2021-09-03 18:22:49 +0700

    add cek pajak kendaraan apps

---
+++ src/tugas3/app.java
@@ -0,0 +1,104 @@
+package tugas3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Kendaraan {
+    double pajak;
+    int harga;
+
+    public Kendaraan(int harga){
+        this.harga = harga;
+    }
+
+    public double cekPajak() {
+        return pajak;
+    }
+}
+
+class Mobil extends Kendaraan {
+    public Mobil(int harga) {
+        super(harga);
+    }
+
+    public double cekPajak() {
+        this.pajak = 0.05 * this.harga;
+        return super.cekPajak();
+    }
+}
+
+class Motor extends Kendaraan {
+    public Motor(int harga) {
+        super(harga);
+    }
+
+    public double cekPajak() {
+        this.pajak = 0.025 * harga;
+        return super.cekPajak();
+    }
+}
+
+class Bis extends Kendaraan {
+    public Bis(int harga) {
+        super(harga);
+    }
+
+    public double cekPajak() {
+        this.pajak = 0.075 * harga;
+        return super.cekPajak();
+    }
+}
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di Mesin Penghitung Pajak!\n" +
+                "Berikut adalah daftar kendaraan yang dapat dicek,\n" +
+                "- Mobil\n" +
+                "- Motor\n" +
+                "- Bis\n" +
+                "Masukkan jumlah kendaraan yang dimiliki : ");
+
+        Scanner scanner_jumlah = new Scanner(System.in);
+
+        try {
+            String[] listKendaraan = new String[scanner_jumlah.nextInt()];
+
+            System.out.printf("Masukkan jenis kendaraan dan harga dalam format kendaraan, harga. Setiap kendaraan dipisah dengan enter:\n");
+            Scanner scanner_kendaraan = new Scanner(System.in);
+
+            for (int i=0; i < listKendaraan.length; i++) {
+                listKendaraan[i] = scanner_kendaraan.nextLine();
+            }
+
+            double total = 0;
+            for (String input : listKendaraan) {
+                String[] kendaraan = input.split(", ");
+                int harga = 0;
+
+                if(kendaraan[0].toLowerCase().equals("mobil")){
+                    harga = Integer.parseInt(kendaraan[1]);
+                    Mobil mobil = new Mobil(harga);
+                    total += mobil.cekPajak();
+                }
+                else if(kendaraan[0].toLowerCase().equals("motor")){
+                    harga = Integer.parseInt(kendaraan[1]);
+                    Motor motor = new Motor(harga);
+                    total += motor.cekPajak();
+                }
+                else if(kendaraan[0].toLowerCase().equals("bis")){
+                    harga = Integer.parseInt(kendaraan[1]);
+                    Bis bis = new Bis(harga);
+                    total += bis.cekPajak();
+                }
+                else {
+                    System.out.println("Maaf, jenis kendaraan " + kendaraan[0] + " belum terdaftar");
+                }
+            }
+            System.out.println("Berikut adalah total pajak kamu : Rp" + total);
+        }
+        catch (InputMismatchException e){
+            System.out.println("Masukkan jumlah kendaraan dalam angka");
+        }
+
+    }
+}
